dd0648fe8d2a5c5fce3ad0fd3981b12f0c895816
Gunin Wasan <98042082+guninwasan@users.noreply.github.com>
2022-06-07 16:20:52 +0530

    Created a file which changes API Key
    If one API doesn't work, then this function changes to another API

---
+++ Config/changeAPI.js
@@ -0,0 +1,13 @@
+async function changeAPIkey() {
+  if (api_key == '0dc69a25699642818e85027cf061af39') {
+    api_key = 'cc8622f37389464482847d2cd5ee14e1';
+  }
+  else if (api_key == 'cc8622f37389464482847d2cd5ee14e1') {
+    api_key = '7745b62256a546cd8eb3390aec336498';
+  }
+  else if (api_key == '7745b62256a546cd8eb3390aec336498') {
+    api_key = '0dc69a25699642818e85027cf061af39';
+  }
+}
+
+export default changeAPIkey;
